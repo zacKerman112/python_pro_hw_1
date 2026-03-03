fadf6adccb802c7b0d88cfdd5b277aa2fcd55825
Zachar Dovzhanskyi <zac.dev07@gmail.com>
2026-03-03 19:55:36 +0200

    Added indentations

--- rect_task.py
+++ rect_task.py
@@ -1,5 +1,6 @@
 import math
 
+
 # 1. Circle Program
 def area(radius: float) -> float:
     """
@@ -19,26 +20,32 @@ try:
 except:
     print("Something went wrong , enter a valid number so i can calculate an area of a circle for you")    
 
+
 # 2. Rectangle Class
 class Rectangle:
     """A class used to represent a Rectangle geometry."""
+
 
     def __init__(self, width: float, height:float):
         """Initialises the rectangle with height and width"""
         self.width = width
         self.height = height
 
+
     def rect_area(self) -> float:
         """Calculates area by multiplying height by width"""
         return self.height * self.width
+
     
     def rect_perimeter(self) -> float:
         """Calculates perimeter with height plus width doubled"""
         return 2 * (self.height + self.width)
+
     
     def is_square(self) -> bool:
         """Checks if the rectangle with given sides is square"""
         return self.height == self.width
+
     
     def resize(self, new_width: float, new_height: float) -> None:
         """Updates rectangle dimentions.
@@ -49,6 +56,7 @@ class Rectangle:
         self.height = new_height
         self.width = new_width
         print(f"The rectangle is resized to {new_height}x{new_width}")
+
 
 # 3. Testing
 rect = Rectangle(8 , 9)
